6a3fa1917c375ea36cddbeb70e4088c9d61207df
Dejan Savic <savic.dejan.business@gmail.com>
2024-01-02 09:48:43 -0600

    executable terminal file added

---
+++ iris/terminal.py
@@ -0,0 +1,19 @@
+from utilities import customized_response
+
+
+def term():
+    user_input = input("Ask a question or press 1 to exit:\n")
+    while user_input != 1:
+        if user_input == '1':
+            break
+        else:
+            try:
+                response = customized_response(user_input)
+                print("\nresponse:\n" + response + "\n")
+                user_input = input("Ask another question or press 1 to exit:\n")
+            except Exception:
+                print("failed to get a response\n")
+                user_input = input("Ask another question or press 1 to exit:\n")
+
+
+term()

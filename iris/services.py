e12e2eb9bcad211f7a1356853b763962c12d3e1b
Kevin McNamara <kevin@mcnamara-group.com>
2024-04-19 16:56:21 -0400

    added torch and GPU offloading via MPS.  Please change to CPU or CUDA where applicable

--- iris/services.py
+++ iris/services.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch as tp
 
 from rag_utilities import embedd, load_pickle, semantic_search, save_pickle
 from utilities import file_checker, customized_response, voice_transcription, text_to_speech, get_audio_file, \
@@ -66,19 +67,16 @@ def file_semantic_search(query):
         # have an array of title and content embeddings
         for key, value in files.items():
             key_title_content.append([key, value["title_embeddings"], value["content_embeddings"]])
-        print("Appended Embeddings")
+        #print("Appended Embeddings")
         # Title Embedding Corpus
-        title_embeddings = np.array([x[1].cpu().numpy() for x in key_title_content])
-        content_embeddings = np.array([x[2].cpu().numpy() for x in key_title_content])
-        #title_embeddings = np.array([x[1] for x in key_title_content])
-        # Content Embedding Corpus
-        #content_embeddings = np.array([x[2] for x in key_title_content])
-        print("Got to torch stacks")
+        title_embeddings = tp.stack([x[1].to(device="mps") for x in key_title_content])
+        content_embeddings = tp.stack([x[2].to(device="mps") for x in key_title_content])
+        #print("Got to torch stacks")
         # Semantic Search for top 2 of Title Embeddings > 50% add to matches
         matches = [hit for hit in semantic_search(query_embedding, title_embeddings, 3) if hit['score'] >= .2]
         # Semantic Search for top 2 of Content Embeddings > 40% add to matches
         matches.extend([hit for hit in semantic_search(query_embedding, content_embeddings, 3) if hit['score'] >= .2])
-        print("got to hits")
+        #print("got to hits")
         #  Insert Matches into a dictionary using corpus_id as key and score as value
         for match in matches:
             corp_id = match.get('corpus_id', "not found")
